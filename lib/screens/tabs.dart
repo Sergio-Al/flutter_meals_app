8499682d573f6b6c526b36a66b8339fdfaec411b
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-07-15 16:12:38 -0400

    Reading & Using Returned Data

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -49,14 +49,17 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
-  void _setScreen(String identifier) {
+  void _setScreen(String identifier) async {
     Navigator.of(context).pop();
     if (identifier == 'filters') {
-      Navigator.of(context).push( // you can use pushReplacement as well
+      final result = await Navigator.of(context).push<Map<Filter, bool>>(
+        // you can use pushReplacement as well
         MaterialPageRoute(
           builder: (ctx) => const FiltersScreen(),
         ),
       );
+
+      print(result);
     }
   }
 
