e6f7d7f59da91e4273ec7ac23cc9c727b14ba89a
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-07-21 11:36:27 -0400

    Creating filters provider

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -4,10 +4,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/filters.dart';
 import 'package:meals_app/screens/meals.dart';
-import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
 import 'package:meals_app/providers/meals_provider.dart';
 import 'package:meals_app/providers/favorites_provider.dart';
+import 'package:meals_app/providers/filters_provider.dart';
 
 const kInitialFilters = {
   // K is by flutter convention for universal variables
